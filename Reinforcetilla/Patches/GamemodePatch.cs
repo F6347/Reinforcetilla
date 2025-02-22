c37f29eeebf7a035241d8fe337aa76469c34625d
F6347 <103911667+F6347@users.noreply.github.com>
2025-02-22 14:03:38 -0500

    Fixed a small bug which prevented you from joining hoverboard lobbies

--- Reinforcetilla/Patches/GamemodePatch.cs
+++ Reinforcetilla/Patches/GamemodePatch.cs
@@ -13,7 +13,7 @@ namespace Reinforcetilla.Patches
         static bool Prefix(GorillaNetworkJoinTrigger __instance, ref string __result)
         {
             HashSet<GameModeType> validGM = GameMode.GameModeZoneMapping.GetModesForZone(__instance.zone, NetworkSystem.Instance.SessionIsPrivate);
-            __result = validGM.Contains(Enum.Parse<GameModeType>(GorillaComputer.instance.currentGameMode.Value.Replace("MODDED_", ""))) ? GorillaComputer.instance.currentGameMode.Value : validGM.Single().ToString();
+            __result = validGM.Contains(Enum.Parse<GameModeType>(GorillaComputer.instance.currentGameMode.Value.Replace("MODDED_", ""))) ? GorillaComputer.instance.currentGameMode.Value : validGM.First().ToString();
             return false;
         }
     }
